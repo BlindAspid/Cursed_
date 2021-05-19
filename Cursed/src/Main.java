10819106277b004657cd39eb5e30665a868d98c9
avolk <avolk@homeaway.com>
2021-05-18 20:54:58 -0500

    Some pointers

--- Cursed/src/Main.java
+++ Cursed/src/Main.java
@@ -2,7 +2,12 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-
-        }
+        Vector<Integer> testVector = new Vector<>(1, 2, 3, 4, 5);
+        System.out.printf("first: %d\n", testVector.front());
+        System.out.printf("last: %d\n", testVector.back());
+        System.out.printf("third: %d\n", testVector.get(2));
+        System.out.printf("size: %d\n", testVector.size());
+        //Test it
     }
+}
 
